305dad97e555e06eb3c750fa8e2c944e1364df2c
Benedict Osei Sefa <newlyf38@gmail.com>
2022-07-18 11:23:06 +0000

    done wit the media configuration

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
