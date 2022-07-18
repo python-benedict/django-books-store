6151a410b4d11facf2ed733d5c6963f7816b849c
Benedict Osei Sefa <newlyf38@gmail.com>
2022-07-18 11:39:26 +0000

    created admin.py file for the category and product

--- store/admin.py
+++ store/admin.py
@@ -4,6 +4,14 @@ from .models import Category, Product
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
+    list_filter =  ['is_active', 'in_stock']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug':('title',)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product)
